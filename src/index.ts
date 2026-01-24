9d2332bd70a40c236ff1664f7fb7bc7ba71fc980
KocouEnot <kocojlanblu5@gmail.com>
2026-01-24 16:53:53 +0100

    Added more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {};
+import { Command } from 'commander'
 
-console.log("wow!!!!!!");
+// Declare the program
+
+const program = new Command()
+
+// Add actions into that CLI
+
+program
+    .argument('<string>', 'string to split')
+    .option("-c, --capitalize", "Capitalize the message")
+    .action(
+        (
+            message: string,
+            opts: {
+                capitalize?: boolean
+            },
+        ) => {
+            if (opts.capitalize) {
+                console.log(message.toUpperCase())
+            } else {
+                console.log(message)
+            }
+        },
+    )
+    .description('Say hello')
+
+program
+    .command('add <numbers...>')
+    .action((numbers: number[]) => {
+        const total = numbers.reduce((a, b) => a + b, 0)
+        console.log(`Total: ${total}`)
+    })
+    .description('Add numbers and log the total')
+
+program
+    .command('get-max-number <numbers...>')
+    .action((numbers: number[]) => {
+        const max = Math.max(...numbers)
+        console.log(`Max: ${max}`)
+    })
+    .description('Add numbers and log the total')
+
+
+// Execute the CLI with the given arguments
+
+program.parse(process.argv)
